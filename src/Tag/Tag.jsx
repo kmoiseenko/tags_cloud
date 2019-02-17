154baa74bfe0dee85e76587525c49ecdf0bfbd8c
Konstantin Moiseenko <write.to.k.moiseenko@gmail.com>
2019-02-17 17:27:02 +0200

    Edit tag view. Add some functions for cases when we dont have data. Add some styles

--- src/Tag/Tag.jsx
+++ src/Tag/Tag.jsx
@@ -5,32 +5,47 @@ import './Tag.css';
 import API from '../data/tags_cloud.json';
 
 const Tag = ({ match }) => {
-	const tagIdFromParams = match.params.id;
-	let tag = API.find(item => item.id === tagIdFromParams);
+	const tagIdFromParams = decodeURIComponent(match.params.id);
+	const tag = API.find(item => item.id === tagIdFromParams);
 
 	if(tag) {
-		let totalMentions = Object.values(tag.sentiment).reduce((sum, value) => {
-			return sum + value;
+		const totalMentions = Object.values(tag.sentiment).reduce((sum, value) => {
+			return sum += value;
+		}, 0);
+		const mentions = Object.keys(tag.sentiment).map(key => {
+			const val = tag.sentiment[key];
+			return(
+				<div key={key} className="tag-row">
+					<div>
+						<span className="text-cap">{key}</span> mentions:
+					</div>
+					<div><b>{val}</b></div>
+				</div>
+			);
 		});
 		const pageTypeList = Object.keys(tag.pageType).map(key => {
 			const val = tag.pageType[key];
-			return <li key={key}>{key}: {val}</li>
+			return(
+				<div key={key} className="tag-row">
+					<div>{key}:</div>
+					<div><b>{val}</b></div>
+				</div>
+			);
 		});
 
 		return(
-			<ul>
-				<li>Label: {tag.label}</li>
-				<li>Total mentions: {totalMentions}</li>
-				<li>Positive Mentions: {tag.sentiment.positive}</li>
-				<li>Neutral Mentions: {tag.sentiment.neutral}</li>
-				<li>Negative Mentions: {tag.sentiment.negative}</li>
-				<li>
-					List of page types:
-					<ul>
-						{pageTypeList}
-					</ul>
-				</li>
-			</ul>
+			<div className="tag">
+				<h1 className="text-center">{tag.label}</h1>
+				<div className="tag-info">
+					<div className="tag-row">
+						<div>Total mentions:</div>
+						<div><b>{totalMentions}</b></div>
+					</div>
+					{mentions}
+					<h3 className="text-center">List of page types</h3>
+					{pageTypeList}
+				</div>
+			</div>
 		);
 	} else {
 		return <h1>Tag was not found</h1>
