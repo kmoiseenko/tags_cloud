2e0e09577e6ba7ef3e6a86116c74ab622f2ff4c1
Konstantin Moiseenko <write.to.k.moiseenko@gmail.com>
2019-02-17 15:04:01 +0200

    Remove useless imports. Add statement for cases when there is wrong id of tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, BrowserRouter, Route, Redirect, Link} from 'react-router-dom';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
 
 import Home from './Home/Home';
 import Tag from './Tag/Tag';
--- src/Tag/Tag.jsx
+++ src/Tag/Tag.jsx
@@ -4,44 +4,49 @@ import './Tag.css';
 
 import API from '../data/tags_cloud.json';
 
-const Tag = (props) => {
-	const id = props.location.pathname.split('/').pop();
+const Tag = ({ match }) => {
+	const tagId = match.params.id;
 	let tag = null;
-	let TotalMentions = null;
-	let pageTypeList = [];
 
 	for(let i = 0; i < API.length; i++) {
-		if(API[i].id === id) {
+		if(API[i].id === tagId) {
 			tag = API[i]
 			break;
 		}
 	}
 
-	for(let key in tag.sentiment) {
-		TotalMentions += tag.sentiment[key]
-	}
+	if(tag) {
+		let TotalMentions = null;
+		let pageTypeList = [];
 
-	for(let key in tag.pageType) {
-		pageTypeList.push(
-			<li key={id + key}>{key}: {tag.pageType[key]}</li>
+		for(let key in tag.sentiment) {
+			TotalMentions += tag.sentiment[key]
+		}
+
+		for(let key in tag.pageType) {
+			pageTypeList.push(
+				<li key={tagId + key}>{key}: {tag.pageType[key]}</li>
+			);
+		}
+
+		return(
+			<ul>
+				<li>Label: {tag.label}</li>
+				<li>Total mentions: {TotalMentions}</li>
+				<li>Positive Mentions: {tag.sentiment.positive}</li>
+				<li>Neutral Mentions: {tag.sentiment.neutral}</li>
+				<li>Negative Mentions: {tag.sentiment.negative}</li>
+				<li>
+					List of page types:
+					<ul>
+						{pageTypeList}
+					</ul>
+				</li>
+			</ul>
 		);
+	} else {
+		return <h1>Tag was not found</h1>
 	}
-
-	return (
-		<ul>
-			<li>Label: {tag.label}</li>
-			<li>Total mentions: {TotalMentions}</li>
-			<li>Positive Mentions: {tag.sentiment.positive}</li>
-			<li>Neutral Mentions: {tag.sentiment.neutral}</li>
-			<li>Negative Mentions: {tag.sentiment.negative}</li>
-			<li>
-				List of page types:
-				<ul>
-					{pageTypeList}
-				</ul>
-			</li>
-		</ul>
-	);
 }
 
 export default Tag;
